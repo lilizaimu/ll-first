3ae9abf4731293eec4653a638171cd4f6648d675
lilizaimu <shandongqdlpl@163.com>
2017-02-19 12:26:39 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/administrator/newkj/MainActivity.java
+++ app/src/main/java/com/example/administrator/newkj/MainActivity.java
@@ -30,7 +30,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    int picsize=33;
+    int picsize=313;
     private int page=1;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
